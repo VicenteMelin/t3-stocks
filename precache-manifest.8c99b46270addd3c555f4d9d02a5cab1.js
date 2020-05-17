7b7a0ca7a8a53626b87b1f3510489ae62b25c753
VicenteMelin <vtmelin@uc.cl>
2020-05-17 17:12:48 -0400

    Updates

---
+++ precache-manifest.8c99b46270addd3c555f4d9d02a5cab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc48dc46d753f328959fae44455c022",
+    "url": "/t3-stocks/index.html"
+  },
+  {
+    "revision": "b94b789272cc5d83349c",
+    "url": "/t3-stocks/static/css/main.fec874d2.chunk.css"
+  },
+  {
+    "revision": "bc83710ffe6dba936ed9",
+    "url": "/t3-stocks/static/js/2.ed0b4a6d.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/t3-stocks/static/js/2.ed0b4a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94b789272cc5d83349c",
+    "url": "/t3-stocks/static/js/main.69c04875.chunk.js"
+  },
+  {
+    "revision": "35b831650bec0ebc545a",
+    "url": "/t3-stocks/static/js/runtime-main.2a66708f.js"
+  }
+]);
